32a075c52f99122240e08e4a6af7f49586650b8e
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2022-01-28 23:08:48 -0300

    chore: create a basic theme

---
+++ styles/theme.ts
@@ -0,0 +1,39 @@
+export const defaultTheme = {
+  colors: {
+    redAccent: "#9e2b2f",
+    goldenBorder: '#eabb6e',
+    blackText: "#281305",
+    blackFadedText: "#2c3041",
+    black: "#000000",
+    linkHoverBlue: "#090962",
+    boxHoverBlue: "#2896db",
+    grayBackground: "#f5f3f4"
+  },
+  fonts: {
+    body: '"Gentium Basic", "Garamond", serif',
+  },
+  fontWeight: {
+    bold: 700,
+    regular: 400,
+  },
+  fontSize: {
+    xxxs: 12.006,
+    xxs: 14.2258,
+    xs: 15.75,
+    sm: 16.002,
+    normal: 18,
+    md: 21.402,
+    lg: 28,
+  },
+  lineHeight: {
+    md: 26,
+    sm: 16.0002,
+
+  },
+  spacing: {
+    xs: 5,
+    sm: 10,
+    lg: 20,
+    xl: 42.2358
+  }
+};
