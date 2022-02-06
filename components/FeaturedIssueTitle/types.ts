208ba0043b8a9c0812680e5689a288cddea28679
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2022-02-06 19:34:00 -0300

    feat(docs): document props using comments

--- components/FeaturedIssueTitle/types.ts
+++ components/FeaturedIssueTitle/types.ts
@@ -1,8 +1,23 @@
 export type FeaturedIssueTitleProps =
   React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+    /**
+     * The organizers of the issue
+     */
     organizers: string
+    /**
+     * The issue semester
+     */
     issueSemester: string
+    /**
+     * The last date that this issue is accepting papers
+     */
     acceptingPapersUntil?: Date | number
+    /**
+     * The issue subtitle
+     */
     subtitle?: string
+    /**
+     * The issue title
+     */
     title: string
   }
