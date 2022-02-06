9f58c1e90645630ec3e08b3ccdf3e3689ae909b9
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2022-02-06 19:33:46 -0300

    chore: use @storybook/addon-docs addon to facilitate documentation

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
   stories: ["../**/*.stories.mdx", "../**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-docs",
+    "@storybook/addon-essentials",
+    "@storybook/addon-links",
+  ],
   framework: "@storybook/react",
   core: {
     builder: "webpack5",
