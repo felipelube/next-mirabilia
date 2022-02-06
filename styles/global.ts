c284539ef0e47b62a4d129df1febab4fc7522cd0
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2022-02-06 12:34:45 -0300

    feat: add and use tailwind's CSS reset

--- styles/global.ts
+++ styles/global.ts
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from "styled-components"
+import "./reset.css"
 
 export const GlobalStyle = createGlobalStyle`
+
   body {
     font-family: ${({ theme }) => theme.fonts.body};
   }
