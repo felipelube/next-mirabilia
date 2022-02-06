992178e92c86f59994dc376ab794dc75a5fc0810
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2022-02-06 12:19:42 -0300

    feat: add basic story

---
+++ components/FeaturedIssueTitle/FeaturedIssueTitle.stories.tsx
@@ -0,0 +1,29 @@
+// FeaturedIssueTitle.stories.ts|tsx
+
+import React from "react"
+
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+
+import { FeaturedIssueTitle } from "./FeaturedIssueTitle"
+import type { FeaturedIssueTitleProps } from "."
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "FeaturedIssueTitle",
+  component: FeaturedIssueTitle,
+} as ComponentMeta<typeof FeaturedIssueTitle>
+
+const defaultProps: FeaturedIssueTitleProps = {
+  organizers: "Fulano de tal",
+  issueSemester: "Libanus 01 (2022/2)",
+  subtitle:
+    "Incididunt aliquip labore Lorem ipsum enim eiusmod tempor anim id culpa.",
+  title: "Occaecat cupidatat fugiat non voluptate do.",
+}
+
+export const Primary: ComponentStory<typeof FeaturedIssueTitle> = () => (
+  <FeaturedIssueTitle {...defaultProps}>FeaturedIssueTitle</FeaturedIssueTitle>
+)
