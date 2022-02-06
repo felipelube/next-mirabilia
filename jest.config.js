d1a4dc6b3639f0c6f04002ad4980904c753ee912
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2022-02-05 22:26:10 -0300

    chore: ignore purely cosmetic components from test coverage

--- jest.config.js
+++ jest.config.js
@@ -17,6 +17,7 @@ const makeCustomJestConfig = () => {
     // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
     moduleDirectories: ["node_modules", "<rootDir>/"],
     testEnvironment: "jest-environment-jsdom",
+    coveragePathIgnorePatterns: ["/node_modules/", "\\.styles\\.[^\\/]+$"],
     transform: {
       [testPattern]: isCollectingTestCoverage
         ? ["babel-jest", { presets: ["next/babel"] }]
