72c8896c1e91d28956d461f490a8a831abea1b24
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2022-02-17 23:49:39 -0300

    refactor: use real-world values

--- components/ArticleListItem/ArticleListItem.stories.tsx
+++ components/ArticleListItem/ArticleListItem.stories.tsx
@@ -17,24 +17,24 @@ export default {
 } as ComponentMeta<typeof ArticleListItem>
 
 const defaultProps: ArticleListItemProps = {
-  originalTitle: "The Diablo in la sierra",
+  originalTitle:
+    " O conflito religioso no século V por meio de duas visões paralelas – O saque de Roma, de 410 d.C., em duas obras literárias: De Reditu suo, de Rutilio Namaziano, e A Cidade de Deus, de Santo Agostinho",
   abstract:
-    "Voluptate laboris non proident minim consequat ullamco deserunt sint nulla Lorem eu. Amet ut occaecat ut enim veniam aute eiusmod elit aliqua. In enim excepteur consequat ipsum velit id pariatur laboris ipsum ipsum. Lorem cupidatat aute proident magna ut non officia ipsum est.",
+    "This article wants to analyze the event known as The Sack of Rome, occurred on 410 DC, using two contemporary works: De Reditu Suo, of the pagan poet Rutilio Namaziano and The City of God, written by the bishop Agostine from Hipona. The choice of these authors aimed to draw a religious parallel that illustrates the theological conflict that existed in that time. It will be initially presented the historical context preceding the period in question, known as Late Antiquity. Will be presented the political, social, economic and military questions, in order to understand the religious and social conflict caused by the rise of Christianity and the consequent decline of paganism. Is important to remember that this article is not intended to be a theological or literary thesis about the works that we are studying here. It is only a superficial historical view of an extremely wide and rich period, too complex to be treated in a few pages.",
   keywords: [
-    "magna",
-    "exercitation",
-    "laboris",
-    "consequat",
-    "mollit",
-    "enim",
-    "sit",
-    "sint",
+    "Christianity",
+    "Late Antiquity",
+    "Paganism",
+    "Sack of Rome",
+    "Theological conflict..",
   ],
-  authors: "Ricardo Montana and Baldur Udur",
-  title: "Est in pariatur magna ea eu",
+  authors: "Bento SILVA SANTOS, Filicio MULINARI",
+  title:
+    "Religious conflict in the fifth century through two parallel views – The sack of Rome in 410 AD in two literary works: De Reditu suo of Rutilio Namaziano and The City of God, of St. Augustine",
   url: "https://www.google.com",
-  issueTitle: "Baldur bests",
-  issueURL: "https://www.google.com",
+  issueTitle:
+    "Relations between History and Literature in Ancient and Medieval World",
+  issueURL: "https://www.revistamirabilia.com/issues/mirabilia-13-2011-2",
 }
 
 const Template: ComponentStory<typeof ArticleListItem> = (args) => (
