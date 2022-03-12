3bb1c1a58efa586d69e6ce07c3a3451410d97cca
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2022-03-12 20:16:19 -0300

    feat: add SiteInfo content

--- components/footer/SiteInfo/SiteInfo.tsx
+++ components/footer/SiteInfo/SiteInfo.tsx
@@ -2,5 +2,28 @@ import Image from "next/image"
 
 export function SiteInfo() {
   return (
+    <div>
+      <a
+        id="ivitra"
+        href="http://www.ivitra.ua.es/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          title="Ivitra - Institut Virtual Internacional de Traducción"
+          src="/images/logoivitraisic.png"
+          alt="Ivitra - Institut Virtual Internacional de Traducción"
+          width="125"
+          height="100"
+        />
+      </a>
+      <h1>ISSN 1676-5818</h1>
+      <p>© 2000 Mirabilia Journal.</p>
+      <p>All rights reserved.</p>
+      <p>
+        <em>Web design</em> by{" "}
+        <a href="https://br.linkedin.com/in/felubra">Felipe Lube</a>.
+      </p>
+    </div>
   )
 }
