8f94dfb0cdf40c49fe17e0afae8dfa37261f79b6
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2022-03-12 18:43:54 -0300

    chore: add components todo

--- layout/Default.tsx
+++ layout/Default.tsx
@@ -1,5 +1,20 @@
 import { ISiteMenu } from "../types/menu-system";
 
 export function DefaultLayout(mainMenu: ISiteMenu) {
-  //TODO:
+  //TODO: Default Layout Architecture
+    //Left sidebar
+    //Main content
+    //Footer
+
+  //TODO: Logo component
+  //TODO: Menu system components
+    //TODO: MenuSection component
+    //TODO: MenuLink component
+  //TODO: SearchBar component
+  //TODO: Footer component
+    //TODO: Ivitra logo
+    //TODO: Facebook like button
+    //TODO: SiteInfo component
+    //TODO: Indexers images
+
 }
