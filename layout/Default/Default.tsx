0504ae120e055bb9518defde3bac59ff994b90ea
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2022-03-22 11:41:50 -0300

    chore: add a link to the logo of the site

--- layout/Default/Default.tsx
+++ layout/Default/Default.tsx
@@ -3,12 +3,20 @@ import * as Layout from "./Default.styles"
 import { Footer } from "../../components/footer/Footer"
 
 import type { DefaultLayoutProps } from "./types"
+import { LogoMirabilia } from "../../components/sidebar/LogoMirabilia"
+import Link from "next/link"
 
 export function DefaultLayout({ mainMenu = [], children }: DefaultLayoutProps) {
   return (
     <Layout.Wrapper>
       <Layout.FirstLine>
-        <Layout.Sidebar mainMenu={mainMenu}></Layout.Sidebar>
+        <Layout.Sidebar>
+          <Link href="/" passHref>
+            <a>
+              <LogoMirabilia />
+            </a>
+          </Link>
+        </Layout.Sidebar>
         <Layout.MainContent>{children}</Layout.MainContent>
       </Layout.FirstLine>
       <Layout.Footer>
